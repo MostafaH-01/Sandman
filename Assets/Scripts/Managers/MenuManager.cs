063495c759207e62172591a78a71d6c4498f6a5e
JKaraky <jawad.alkaraky@gmail.com>
2024-03-02 21:43:14 +0200

    Menu UI

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
